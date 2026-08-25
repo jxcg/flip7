e1104c6218fedc981ae63f963ae6387a16595d9e
jxcg <154761377+jxcg@users.noreply.github.com>
2026-08-25 12:51:35 +0100

    chore: First push to upstream main

---
+++ flip7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flip7
+//
+//  Created by Joshua Ng on 25/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flip7/flip7App.swift
@@ -0,0 +1,17 @@
+//
+//  flip7App.swift
+//  flip7
+//
+//  Created by Joshua Ng on 25/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct flip7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
